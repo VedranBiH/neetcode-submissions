868213087dd5e5c8ec86ab43732e120eb3328889
Vedran Pehlivanovic <vedran.pehlivanovic@icloud.com>
2026-05-27 14:35:13 -0600

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] c = s.toCharArray();
+
+        int i = 0;
+        int y = c.length - 1;
+        System.out.println(i);
+        System.out.println(y);
+        System.out.println(c.length / 2);
+        while(i <= c.length / 2 && y >= c.length / 2){
+
+            if(!Character.isLetterOrDigit(c[i])){
+                i++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(c[y])){
+                y--;
+                continue;
+            }
+            if(Character.toLowerCase(c[i]) != Character.toLowerCase(c[y])){
+                return false;
+            }
+
+            i++;
+            y--;
+        }
+
+        return true;
+    }
+}
